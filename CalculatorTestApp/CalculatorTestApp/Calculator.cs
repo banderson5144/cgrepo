6ba131c1b04714549eec97d1165f4b618cfe7a81
LightAxe2402 <chrisglesmann@gmail.com>
2020-12-05 19:40:47 -0800

    Added Result Files for Question 1&2

---
+++ CalculatorTestApp/CalculatorTestApp/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace CalculatorTestApp
+{
+    public class Calculator
+    {
+        public int Add(int addend1, int addend2)
+        {
+            return addend1 + addend2;
+        }
+
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+
+        }
+
+        public int Multiply(int muliplier, int multiplicand)
+        {
+            return muliplier * multiplicand;
+        }
+
+        public string Divide(int dividend, int divisor)
+        {
+            int whole = (dividend / divisor);
+            int remainder = dividend - (whole * divisor);
+
+            return $"{whole} R{remainder}";
+        }
+    }
+}
---
+++ CalculatorTestApp/CalculatorTestApp/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace CalculatorTestApp
+{
+    class Program
+    {
+        static void PrintDivider() { Console.WriteLine("--------------------------------"); }
+
+		static void GetNumberPair(ref int number1, ref int number2, string label1 = "first number", string label2 = "second number")
+        {
+			Console.Write($"Enter the {label1}: ");
+			number1 = GetValidNumber(label1);
+
+			Console.Write($"Enter the {label2}: ");
+			number2 = GetValidNumber(label2);
+		} 
+
+		static int GetValidNumber(string label)
+        {
+			string str;
+			bool validNumber;
+			int result = 0;
+
+			do
+			{
+				str = Console.ReadLine();
+				validNumber = Int32.TryParse(str, out int newResult);
+
+				if (validNumber)
+					result = newResult;
+				else
+					Console.Write($"Invalid input, Enter the {label}: ");
+			} while (!validNumber);
+
+			return result;
+		}
+
+        static void Main(string[] args)
+        {
+            const char ADD_OPERATOR = '+';
+            const char SUB_OPERATOR = '-';
+            const char MULT_OPERATOR = '*';
+            const char DIV_OPERATOR = '/';
+
+            Calculator calculator = new Calculator();
+            char operationKey;
+            int number1 = 0, number2 = 0;
+
+            PrintDivider();
+            Console.WriteLine("CALCULATOR TEST APP");
+            PrintDivider();
+
+            do
+            {
+                Console.WriteLine("+ :      Add");
+                Console.WriteLine("- : Subtract");
+                Console.WriteLine("* : Multiply");
+                Console.WriteLine("/ :   Divide");
+                Console.WriteLine("Enter any other key to stop execution");
+
+                Console.Write("Enter Operator Here: ");
+                operationKey = Console.ReadKey().KeyChar;
+				Console.WriteLine();
+
+				switch (operationKey)
+				{
+					case ADD_OPERATOR:
+						PrintDivider();
+						GetNumberPair(ref number1, ref number2, "first addend", "second addend");
+
+						Console.WriteLine($"The Result is: {calculator.Add(number1, number2)}!");
+						PrintDivider();
+						Console.WriteLine();
+						break;
+					case SUB_OPERATOR:
+						PrintDivider();
+						GetNumberPair(ref number1, ref number2, "the minuend", "the subtrahend");
+
+						Console.WriteLine($"The Result is: {calculator.Subtract(number1, number2)}!");
+						PrintDivider();
+						Console.WriteLine();
+						break;
+					case MULT_OPERATOR:
+						PrintDivider();
+						GetNumberPair(ref number1, ref number2, "multiplier", "multiplicand");
+
+						Console.WriteLine($"The Result is: {calculator.Multiply(number1, number2)}!");
+						PrintDivider();
+						Console.WriteLine();
+						break;
+					case DIV_OPERATOR:
+						PrintDivider();
+						GetNumberPair(ref number1, ref number2, "dividend", "divisor");
+
+						Console.WriteLine($"The Result is: {calculator.Divide(number1, number2)}!");
+						PrintDivider();
+						Console.WriteLine();
+						break;
+					default:
+						Console.WriteLine("Exiting Execution Now...");
+						break;
+				}
+			} while (operationKey == MULT_OPERATOR || operationKey == SUB_OPERATOR || operationKey == ADD_OPERATOR || operationKey == DIV_OPERATOR);
+        }
+    }
+}
